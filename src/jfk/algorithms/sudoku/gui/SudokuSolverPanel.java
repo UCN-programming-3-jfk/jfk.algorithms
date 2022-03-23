56cb6ca141eed1dcfffe5a7a38920e507e27927d
Jakob Farian Krarup <kr@rup.dk>
2022-03-23 19:18:23 +0100

    Added visual errornotification (yellow background on row/column/quadrant)

--- src/jfk/algorithms/sudoku/gui/SudokuSolverPanel.java
+++ src/jfk/algorithms/sudoku/gui/SudokuSolverPanel.java
@@ -20,6 +20,29 @@ public class SudokuSolverPanel extends JPanel {
 		float f = 55.0f;
 		g.setFont(g.getFont().deriveFont(f));
 		int tileSize = getWidth() / 9;
+		
+		g.setColor(Color.yellow);
+		for (int rowCounter = 0; rowCounter < 9; rowCounter++) {			
+			if(!getSudoku().isRowValid(rowCounter)) {
+				g.fillRect(0, rowCounter * tileSize, tileSize*9, tileSize);
+			}
+		}
+
+		for (int columnCounter = 0; columnCounter < 9; columnCounter++) {			
+			if(!getSudoku().isColumnValid(columnCounter)) {
+				g.fillRect(columnCounter * tileSize, 0, tileSize, tileSize*9);
+			}
+		}
+		
+		for (int quadrantYCounter = 0; quadrantYCounter < 3; quadrantYCounter++) {
+			for (int quadrantXCounter = 0; quadrantXCounter < 3; quadrantXCounter++) {
+				if(!getSudoku().isQuadrantValid(quadrantXCounter, quadrantYCounter)) {
+					g.fillRect(quadrantXCounter * tileSize*3, quadrantYCounter * tileSize * 3, tileSize * 3, tileSize*3);
+				}
+			}
+		}
+
+		
 		for (int rowCounter = 0; rowCounter < 9; rowCounter++) {
 			for (int columnCounter = 0; columnCounter < 9; columnCounter++) {
 				g.setColor(Color.black);
@@ -39,7 +62,7 @@ public class SudokuSolverPanel extends JPanel {
 		}
 		g.setColor(Color.black);
 		Graphics2D graphics2d = (Graphics2D) g;
-		graphics2d.setStroke(new BasicStroke(5));
+		graphics2d.setStroke(new BasicStroke(7));
 		for (int rowCounter = 0; rowCounter < 3; rowCounter++) {
 			for (int columnCounter = 0; columnCounter < 3; columnCounter++) {
 				g.drawRect(columnCounter * tileSize * 3, rowCounter * tileSize * 3, tileSize * 3, tileSize * 3);
